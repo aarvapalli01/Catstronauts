b5d9da69b8aa2ca88744b8a67eb4cf04e882ea1f
Aakash Arvapalli <aakash.arvapalli@gmail.com>
2025-03-29 16:13:38 -0500

    Lift-Off II: Resolvers

---
+++ server/src/resolvers.js
@@ -0,0 +1,16 @@
+const resolvers = {
+    Query: {
+        //returns an array of tracks that will populate the homepage grid
+        //homepage grid of our web client
+        tracksForHome: (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTracksForHome();
+        },
+    },
+    Track: {
+        author: ({authorId}, _, {dataSources}) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        },
+    },
+};
+
+module.exports = resolvers;
