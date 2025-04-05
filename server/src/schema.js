08de48ea6dedc0e215db7e5371172c6915eda50e
Aakash Arvapalli <aakash.arvapalli@gmail.com>
2025-04-05 17:05:50 -0500

    finished Lift-Off III

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    track(id: ID!): Track!
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +20,19 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete descriptions, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "returns an array of Module objects, array cannot be null and entries in array can't be as well"
+    modules: [Module!]!
+  }
+
+  "A module is a single unit of teaching. Multiple Modules compose a track"
+  type Module{
+    id: ID!
+    title: String!
+    length: Int
   }
 
   "Author of a complete Track or a Module"
